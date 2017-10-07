b76164bac2bb5d520e3b5c50494e93cbce55336d
uuushiro <yushiro.ma2ta2.21@gmail.com>
2017-10-08 08:06:12 +0900

    Update init.sh

--- script/init.sh
+++ script/init.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 docker-compose build
-
 docker-compose run --rm web rails new . --force --database=mysql --skip-bundle
-
 cp -f template/database.yml config/database.yml
-
 docker-compose run --rm web bundle exec spring binstub --all
+docker-compose run --rm spring rails db:create
+docker-compose run --rm spring rails db:migrate
+docker-compose up -d
